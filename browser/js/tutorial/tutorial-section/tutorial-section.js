3927ee69d4f21fba72e10116369f84e5ca5027b7
Jamie <jmeshen@gmail.com>
2015-06-24 15:50:01 -0400

    first commit!

---
+++ browser/js/tutorial/tutorial-section/tutorial-section.js
@@ -0,0 +1,14 @@
+app.directive('tutorialSection', function () {
+    return {
+        restrict: 'E',
+        scope: {
+            name: '@',
+            videos: '=',
+            background: '@'
+        },
+        templateUrl: 'js/tutorial/tutorial-section/tutorial-section.html',
+        link: function (scope, element) {
+            element.css({ background: scope.background });
+        }
+    };
+});
