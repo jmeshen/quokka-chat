01480abc781189a2ef11a4bb1bde480982ee0f75
Stan Le <HQ@Toaster.local>
2015-07-02 15:41:54 -0400

    fixed playhead

--- browser/js/common/directives/video-box/video-box.js
+++ browser/js/common/directives/video-box/video-box.js
@@ -5,17 +5,20 @@ app.directive('videoBox', function($rootScope, AuthService, AUTH_EVENTS, $state,
             video: "="
         },
         templateUrl: 'js/common/directives/video-box/video-box.html',
-        link: function(scope,element,attrs) {
+        link: function(scope, element, attrs) {
             scope.embedURL = 'https://youtube.com/embed/' + scope.video.embedId;
             VideoFactory.onYouTubeIframeAPIReady(scope.video.embedId);
             $rootScope.$on('duration', function(event, player) {
+                var videobox = document.getElementsByTagName('video-box');
                 scope.duration = player.getDuration()
                 scope.interval = 5;
                 var playhead = angular.element(document.createElement('playhead'));
-                playhead.attr('duration','duration')
-                playhead.attr('video','video')
+                playhead.attr('duration', 'duration')
+                playhead.attr('video', 'video')
                 var el = $compile(playhead)(scope);
-                var videobox = document.getElementsByTagName('video-box');
+                if (angular.element(videobox[0]).children.length) {
+                    angular.element(videobox[0].lastChild).replaceWith(playhead)
+                }
                 angular.element(videobox[0]).append(playhead)
             })
         }
