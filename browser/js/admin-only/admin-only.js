f3faa293b2a26fdec008442efbc15e8e49ae866b
Jennifer Cui <jcui@Jennifers-MacBook.local>
2015-07-07 18:25:20 -0400

    admin view with user promote

--- browser/js/admin-only/admin-only.js
+++ browser/js/admin-only/admin-only.js
@@ -8,4 +8,30 @@ app.config(function($stateProvider) {
 
 app.controller('AdminController', function($scope, AuthService, UserFactory, $state) {
 
+    $scope.searchingUser = false;
+    $scope.successMessage = false;
+    $scope.failMessage = false;
+
+    $scope.getUser = function(email) {
+        UserFactory.getUserByEmail(email).then(function(user) {
+            $scope.searchingUser = true;
+            $scope.failMessage = false;
+            $scope.foundUser = user;
+        }, function() {
+            $scope.failMessage = true;
+        })
+    }
+
+    $scope.promoteToAdmin = function(status) {
+
+        UserFactory.promoteUserStatus($scope.foundUser._id, {
+            powerLevel: status
+        }).then(function(response) {
+            console.log('ADMIN STATUS CHANGED!');
+            $scope.searchingUser = false;
+            $scope.successMessage = true;
+        });
+
+    };
+
 });
