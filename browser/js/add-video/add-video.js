438e492a5ddd9025f36862472b955ae7c3705bfc
Jennifer Cui <jcui@Jennifers-MacBook.local>
2015-07-09 18:57:39 -0400

    prevents duplicate videos from being added

--- browser/js/add-video/add-video.js
+++ browser/js/add-video/add-video.js
@@ -9,13 +9,25 @@ app.config(function($stateProvider) {
 app.controller('AddVideo', function($scope, AuthService, $state, VideoFactory) {
 
     $scope.addVideo = function() {
+
         $scope.video.embedId = VideoFactory.pullIdFromUrl($scope.video.url);
-        VideoFactory.add($scope.video).then(function(video) {
-            $state.go('aRoom', {
-                id: video._id
-            });
-        }, function(error) {
-            console.log(error);
-        });
+
+        VideoFactory.getVideoByEmbedId($scope.video.embedId).then(function(video) {
+            console.log(video);
+            if (video) {
+                $state.go('aRoom', {
+                    id: video._id
+                });
+            } else {
+                VideoFactory.add($scope.video).then(function(video) {
+                    $state.go('aRoom', {
+                        id: video._id
+                    });
+                }, function(error) {
+                    console.log(error);
+                });
+            }
+        })
+
     }
 });
