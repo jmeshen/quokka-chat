8ab3ad8cf1dee0b03b8c66f98393bc4cf4bf7887
Stan Le <HQ@Toaster.local>
2015-07-09 20:10:19 -0400

    ;alskdfjals;

--- gulpfile.js
+++ gulpfile.js
@@ -86,13 +86,15 @@ gulp.task('testBrowserJS', function(done) {
 });
 
 gulp.task('buildCSS', function() {
-    console.log("HELLO", {
-        includePaths: require('node-bourbon').includePaths
-    })
+    console.log("HELLO",
+        __dirname + '/node_modules/node-bourbon/node_modules/bourbon/app/assets/stylesheets'
+    )
+    // throw Error(require('node-bourbon').includePaths)
+    console.log('hello', require('node-bourbon').includePaths)
     return gulp.src('./browser/scss/main.scss')
         // .pipe(plumber())
         .pipe(sass({
-            includePaths: __dirname + '/node_modules/node-bourbon/node_modules/bourbon/app/assets/stylesheets'
+            includePaths: [__dirname + '/node_modules/node-bourbon/node_modules/bourbon/app/assets/stylesheets']
         }))
         .pipe(rename('style.css'))
         .pipe(gulp.dest('./public'));
