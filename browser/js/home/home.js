46b12b7f4a708e12121e6bf2c1d92aecee9d58ed
Jamie <jmeshen@gmail.com>
2015-07-10 14:04:55 -0400

    scrolling to anchors work

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -1,11 +1,15 @@
 app.config(function($stateProvider) {
-    $stateProvider.state('home', {
-        url: '/',
-        templateUrl: 'js/home/home.html',
-        controller: 'HomeCtrl'
-    });
+  $stateProvider.state('home', {
+    url: '/',
+    templateUrl: 'js/home/home.html',
+    controller: 'HomeCtrl'
+  });
 });
 
-app.controller('HomeCtrl', function($scope) {
-    startAnimation($);
+app.controller('HomeCtrl', function($scope, anchorSmoothScroll) {
+  $scope.scrollToNext = function(eID) {
+    console.log('eID?????',eID)
+    anchorSmoothScroll.scrollTo(eID);
+  }
+  startAnimation($);
 });
