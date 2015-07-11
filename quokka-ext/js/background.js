145c40527c8cebe655f79c3864d27bdf08280cc6
Jennifer Cui <jcui@Jennifers-MacBook.local>
2015-07-11 19:06:13 -0400

    chrome ext posts to web app

--- quokka-ext/js/background.js
+++ quokka-ext/js/background.js
@@ -6,7 +6,12 @@ chrome.runtime.onMessage.addListener(function(req, sender) {
     if (req.command === 'send') {
         $.ajax({
             type: 'POST',
-            url: 'https://www.quokka.chat/api/video/',
+            url: 'http://www.quokka.chat/api/video/',
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+                'Access-Control-Allow-Headers': '*'
+            },
             data: req.data,
             success: function(response) {
                 console.log('Posted successfully!,',
