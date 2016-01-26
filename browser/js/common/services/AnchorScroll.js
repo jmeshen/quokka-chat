5c510e2872030e35184fdc6ab8b18c0f33040535
Stan Le <stan.le1107@gmail.com>
2016-01-25 21:49:18 -0800

    works?"

--- browser/js/common/services/AnchorScroll.js
+++ browser/js/common/services/AnchorScroll.js
@@ -1,4 +1,4 @@
-app.service('anchorSmoothScroll', function(){
+app.service('anchorSmoothScroll', function() {
 
     this.scrollTo = function(eID) {
 
@@ -9,7 +9,8 @@ app.service('anchorSmoothScroll', function(){
         var stopY = elmYPosition(eID);
         var distance = stopY > startY ? stopY - startY : startY - stopY;
         if (distance < 100) {
-            scrollTo(0, stopY); return;
+            scrollTo(0, stopY);
+            return;
         }
         var speed = Math.round(distance / 100);
         if (speed >= 20) speed = 20;
@@ -17,14 +18,19 @@ app.service('anchorSmoothScroll', function(){
         var leapY = stopY > startY ? startY + step : startY - step;
         var timer = 0;
         if (stopY > startY) {
-            for ( var i=startY; i<stopY; i+=step ) {
-                setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
-                leapY += step; if (leapY > stopY) leapY = stopY; timer++;
-            } return;
+            for (var i = startY; i < stopY; i += step) {
+                setTimeout("window.scrollTo(0, " + leapY + ")", timer * speed);
+                leapY += step;
+                if (leapY > stopY) leapY = stopY;
+                timer++;
+            }
+            return;
         }
-        for ( var i=startY; i>stopY; i-=step ) {
-            setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
-            leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
+        for (var i = startY; i > stopY; i -= step) {
+            setTimeout("window.scrollTo(0, " + leapY + ")", timer * speed);
+            leapY -= step;
+            if (leapY < stopY) leapY = stopY;
+            timer++;
         }
 
         function currentYPosition() {
@@ -45,7 +51,8 @@ app.service('anchorSmoothScroll', function(){
             while (node.offsetParent && node.offsetParent != document.body) {
                 node = node.offsetParent;
                 y += node.offsetTop;
-            } return y;
+            }
+            return y;
         }
 
     };
